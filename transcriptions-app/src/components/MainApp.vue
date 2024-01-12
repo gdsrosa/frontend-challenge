6f08f1b39ef44ec789c4dcbebcd335e2e5fdbd95
Gabriel Rosa <gabrieldsrosa@gmail.com>
2024-01-12 17:53:38 -0300

    feat: add styles to match design

--- transcriptions-app/src/components/MainApp.vue
+++ transcriptions-app/src/components/MainApp.vue
@@ -1,9 +1,105 @@
-<script setup lang="ts">
-defineProps<{ message: string }>();
-</script>
-
 <template>
-  <h1>{{ message }}</h1>
+  <div class="main-app">
+    <header class="header">
+      <h2 class="header-title">Transcriptions</h2>
+      <div class="header-items">
+        <button class="button">
+          <img :src="UploadIcon" alt="Button to upload data" />
+        </button>
+        <button class="button">
+          <img :src="FetchDataIcon" alt="Button to get information" />
+        </button>
+      </div>
+    </header>
+    <main class="container">
+      <TranscriptionItem />
+      <TranscriptionItem />
+      <div class="container-item">
+        <button class="button">
+          <img :src="AddRowIcon" alt="Add Row" />
+        </button>
+      </div>
+    </main>
+  </div>
 </template>
 
-<style></style>
+<script setup lang="ts">
+import TranscriptionItem from './TranscriptionItem.vue';
+import FetchDataIcon from '../assets/fetch-document.svg';
+import UploadIcon from '../assets/upload.svg';
+import AddRowIcon from '../assets/add-row.svg';
+</script>
+
+<style scoped>
+.main-app {
+  background-color: #f6f7f8;
+  height: 100vh;
+}
+.button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+.header {
+  align-items: center;
+  background-color: #fff;
+  border-bottom-width: 1px;
+  border: 1px solid #d4d5d6;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 9rem;
+  padding-right: 9rem;
+}
+.header-title {
+  color: #414c5e;
+  font-family: 'Montserrat', sans-serif;
+  padding-bottom: 1rem;
+  padding-top: 1rem;
+}
+
+.header-items {
+  display: flex;
+  justify-content: space-between;
+  width: 4rem;
+}
+
+.container {
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 1rem;
+}
+
+.container-item {
+  display: flex;
+  justify-content: center;
+  padding-top: 1.5rem;
+}
+
+@media (min-width: 640px) {
+  .container {
+    max-width: 640px;
+  }
+}
+@media (min-width: 768px) {
+  .container {
+    max-width: 768px;
+  }
+}
+@media (min-width: 1024px) {
+  .container {
+    max-width: 1024px;
+  }
+}
+@media (min-width: 1280px) {
+  .container {
+    max-width: 1280px;
+  }
+}
+@media (min-width: 1536px) {
+  .container {
+    max-width: 1536px;
+  }
+}
+</style>
---
+++ transcriptions-app/src/components/TranscriptionItem.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="card">
+    <div class="card-item">
+      <input type="checkbox" name="transcription" />
+      <img class="card-item--image" :src="PersonIcon" alt="Drawing of a face" />
+    </div>
+
+    <div class="card-items">
+      <input
+        class="card-items-input-title"
+        type="text"
+        name="title"
+        value="Voice 2"
+      />
+      <input
+        class="card-items-input-description"
+        type="text"
+        name="description"
+        value="This is one line sentence made to show how it fits"
+      />
+    </div>
+    <button class="button">
+      <img :src="RecycleBinIcon" alt="Bin Icon" />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PersonIcon from '../assets/person.svg';
+import RecycleBinIcon from '../assets/delete.svg';
+</script>
+
+<style scoped>
+.button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  visibility: hidden;
+}
+.card {
+  background-color: #fff;
+  border: 1px solid #d4d5d6;
+  display: flex;
+  align-items: center;
+  padding: 0 1.5rem 1.5rem 1.5rem;
+}
+
+.card:hover > .button {
+  visibility: visible;
+}
+
+.card-item {
+  display: flex;
+}
+
+.card-item--image {
+  margin-left: 1rem;
+  margin-right: 0.5rem;
+}
+
+.card-items {
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - 20px);
+}
+
+.card-items-input-title {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: 600;
+  color: #566074;
+  padding-top: 1.5rem;
+  border: none;
+}
+
+.card-items-input-description {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: #778195;
+  border: none;
+}
+</style>
