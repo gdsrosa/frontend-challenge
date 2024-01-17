a3df0de5a407b93cfe0782bc6708db5cf8549fcd
Gabriel Rosa <gabrieldsrosa@gmail.com>
2024-01-17 16:14:22 -0300

    chore: add comment to explain the GET request

--- transcriptions-app/src/api/services/Transcriptions.ts
+++ transcriptions-app/src/api/services/Transcriptions.ts
@@ -2,6 +2,13 @@ import { ENDPOINTS } from '../endpoints';
 import data from '../data.json';
 import { Transcription } from '../types';
 
+/**
+ * This function is not fetching from the endpoint because it has some formatting issue on the JSON
+ * So I've decided to emulate a API response with the payload expected by following this section
+ * on the Readme file:
+ * "Using Mocky should produce no cross origin errors and should work fine for this challenge.
+ * If you do find issues, it's OK to copy the JSON into your preferred method to then provide the data to the app."
+ */
 export async function getTranscriptions(): Promise<Transcription[]> {
   return new Promise<Transcription[]>((resolve, reject) => {
     if (!data) {
@@ -15,7 +22,7 @@ export async function getTranscriptions(): Promise<Transcription[]> {
   });
 }
 
-export async function postTranscriptions(payload: Transcription) {
+export async function postTranscriptions(payload: Transcription[]) {
   await fetch(ENDPOINTS.TRANSCRIPTIONS, {
     method: 'POST',
     body: JSON.stringify(payload),
